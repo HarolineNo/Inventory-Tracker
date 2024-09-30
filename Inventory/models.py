35aa2b0ca768f5119a041561a2d32998e3ea3b14
harolinenoesi <harolinenoesi@gmail.com>
2024-09-30 01:20:27 -0400

    Added views and html files

--- Inventory/models.py
+++ Inventory/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.CharField(max_length=50)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    item = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.item
+
+class RecipeRequirement(models.Model):
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.item} requires {self.quantity} of {self.ingredient}"
+
+class Purchase(models.Model):
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.item} was purchased on {self.timestamp}"
+    
--- Inventory/views.py
+++ Inventory/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.vivews.generic import ListView
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
+from django.db.models import Sum
 
 # Create your views here.
+
+class InventoryView(ListView):
+    model = Ingredient
+    template = 'inventory/templates/inventory.html'
+    name = 'Ingredients'
+
+class MenuView(ListView):
+    model = MenuItem
+    template = 'inventory/templates/menu.html'
+    name = 'Menu'
+
+class RecipeView(ListView):
+    model = RecipeRequirement
+    template = 'inventory/templates/recipe.html'
+    name = 'Recipe Requirements'
+
+class PurchaseView(ListView):
+    model = Purchase
+    template = 'inventory/templates/purchase.html'
+    name = 'Purchases'
+
+    def total_revenue(request):
+        revenue = Purchase.objects.aggregrate(total=models.Sum('price'))
+
+        return render(request, 'inventory/template/purchase.html')
