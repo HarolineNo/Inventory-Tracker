fe113b44d103efc8852f1060ad966d5a11bd329e
harolinenoesi <harolinenoesi@gmail.com>
2024-10-01 00:29:19 -0400

    Upgradded css styles, added sidebar with links, created html files for forms

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import InventoryView, MenuView, RecipeView, PurchaseView
 
 urlpatterns = [
-    path('ingredients/', InventoryView.as_view()),
-    path('menu/', MenuView.as_view()),
-    path('recipes/', RecipeView.as_view()),
-    path('purchases/', PurchaseView.as_view())
+    path('ingredients/', InventoryView.as_view(), name='ingredients_list'),
+    path('menu/', MenuView.as_view(), name='menu_items'),
+    path('recipes/', RecipeView.as_view(), name='recipe_requirements'),
+    path('purchases/', PurchaseView.as_view(), name='purchase_log')
 ]
